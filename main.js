e6dc006b21fe0c60ae0cf35fdc6f13d56a3e0511
Alex Riviere <alex@appliedimagination.com>
2016-12-02 18:39:45 -0500

    made some improvements to my storage solution

--- main.js
+++ main.js
@@ -1,49 +1,67 @@
 var newToDoFront = "<li class='task mdl-list__item'><div class='mdl-list__item-primary-content mdl-card mdl-shadow--2dp'><div class='mdl-card__title'><h2 class='mdl-card__title-text'>Todo</h2></div><div class='mdl-card__supporting-text'><textarea class='mdl-textfield__input'>";
 var newToDoEnd = "</textarea></div><div class='mdl-card__actions mdl-card--border'></div><div class='mdl-card__menu'><button class='close mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect'><i class='material-icons'>remove</i></button></div></div></li>";
-var task_lists = {"todo": "#todolist",
-                 "working": "#workinglist",
-                 "completed": "workinglist"};
 var task_ID = 0;
 
-function addToDoTask(tasklist, contents){
-	$(task_lists[tasklist]).append(newToDoFront+contents+newToDoEnd);
-	$("button.close").on("click", function () {
-		$(this).closest("li").remove();
-	});
+function swap(json){
+  var ret = {};
+  for(var key in json){
+    ret[json[key]] = key;
+  }
+  return ret;
+}
+var tl = {
+"todo": "#todolist",
+"working": "#workinglist",
+"completed": "workinglist"
+};
+var lt = swap(tl);
+
+
+function addToDoTask(tasklist, contents) {
+    $(tl[tasklist]).append(newToDoFront + contents + newToDoEnd);
+    $("button.close").on("click", function() {
+        $(this).closest("li").remove();
+    });
+    $("textarea").on("update")
 }
 
-function getListInfo(selector){
+function getListInfo(selector) {
     var ret = new Array();
-    var allTextElements = $(selector+" textarea");
-    for(var a =0; a < $(allTextElements).length; a++){
+    var allTextElements = $(selector + " textarea");
+    for (var a = 0; a < $(allTextElements).length; a++) {
         ret.push($($(allTextElements)[a]).val());
     }
     return ret;
 }
 
-function updateCookies(event, ui) {
-    Cookies.set("lists", {todo: getListInfo("#todolist"),
-                         working: getListInfo("#workinglist"),
-                         completed: getListInfo("#completedlist") }, {expires: 365});
-    Cookies.set("task_ID", task_ID, {expires: 365});
+function updateStorage(event, ui) {
+    console.log(event);
+    console.log(ui);
+    var tasklist = "#"+event.currentTarget.id;
+    updateLocalStorage(tasklist);
+}
+
+function updateLocalStorage(tasklist){
+	localStorage.setItem(lt[tasklist], JSON.stringify(getListInfo(tasklist)));
+}
+
+function restoreCookie() {
 
 }
 
-function restoreCookie(){
+$(document).ready(function() {
 
-}
-
-$(document).ready(function () {
-
-    $("button#addnew").on("click", function () {addToDoTask("todo");});
+    $("button#addnew").on("click", function() {
+        addToDoTask("todo", "");
+    });
 
     $("ul.sortable").sortable({
         connectWith: "ul",
         revert: true
     });
 
-    $("ul.sortable").on("sortchange", function(event,ui){updateCookies(event,ui)});
-    $("ul.sortable").on("sortupdate", function(event,ui){updateCookies(event,ui)});
-
+    $("ul.sortable").on("sortupdate", function(event, ui) {
+        updateStorage(event, ui)
+    });
 
 });
