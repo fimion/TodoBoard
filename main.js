caa85ea31bed94236211d25b1e1c8fc1b65045c8
Alex Riviere <fimion@gmail.com>
2016-12-01 19:34:12 -0500

    added cookie storage. Still working on the final result of that.

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-var newToDo = "<li class='draggable'><button class='close fa fa-times'></button><textarea class='mui--z5'> </textarea></li>";
+var newToDoFront = "<li class='task'><button class='close fa fa-times'></button><textarea class='mui--z5'>";
+var newToDoEnd = "</textarea></li>";
+var cookie_lists = {"todo": "#todolist",
+                 "working": "#workinglist",
+                 "completed": "workinglist"};
+var task_ID = 0;
+
+function getListInfo(selector){
+    var ret = new Array();
+    var allTextElements = $(selector+" textarea");
+    for(var a =0; a < $(allTextElements).length; a++){
+        ret.push($($(allTextElements)[a]).val());
+    }
+    return ret;
+}
+
+function updateCookies(event, ui) {
+    Cookies.set("lists", {todo: getListInfo("#todolist"),
+                         working: getListInfo("#workinglist"),
+                         completed: getListInfo("#completedlist") }, {expires: 365});
+    Cookies.set("task_ID", task_ID, {expires: 365});
+
+}
+
+function restoreCookie(){
+
+}
 
 $(document).ready(function () {
 
     $("button.addnew").on("click", function () {
-        $("#todolist").append(newToDo);
+        $("#todolist").append(newToDoFront+newToDoEnd);
         $("button.close").on("click", function () {
             $(this).parent("li").remove();
         });
@@ -13,6 +39,10 @@ $(document).ready(function () {
         connectWith: "ul",
         revert: true
     });
+
+    $("ul.sortable").on("sortchange", function(event,ui){updateCookies(event,ui)});
+    $("ul.sortable").on("sortupdate", function(event,ui){updateCookies(event,ui)});
+
 
 });
 
