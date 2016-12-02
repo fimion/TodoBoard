07c3f5b09835a03458249a3941908c52f30f346b
Alex Riviere <fimion@gmail.com>
2016-12-02 08:06:20 -0500

    updated design to MDL

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var newToDoFront = "<li class='task'><button class='close fa fa-times'></button><textarea class='mui--z5'>";
-var newToDoEnd = "</textarea></li>";
+var newToDoFront = "<li class='task mdl-list__item'><div class='mdl-list__item-primary-content mdl-card mdl-shadow--2dp'><div class='mdl-card__title'><h2 class='mdl-card__title-text'>Todo</h2></div><div class='mdl-card__supporting-text'><textarea class='mdl-textfield__input'>";
+var newToDoEnd = "</textarea></div><div class='mdl-card__actions mdl-card--border'></div><div class='mdl-card__menu'><button class='close mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect'><i class='material-icons'>remove</i></button></div></div></li>";
 var cookie_lists = {"todo": "#todolist",
                  "working": "#workinglist",
                  "completed": "workinglist"};
@@ -28,10 +28,10 @@ function restoreCookie(){
 
 $(document).ready(function () {
 
-    $("button.addnew").on("click", function () {
+    $("button#addnew").on("click", function () {
         $("#todolist").append(newToDoFront+newToDoEnd);
         $("button.close").on("click", function () {
-            $(this).parent("li").remove();
+            $(this).closest("li").remove();
         });
     });
 
